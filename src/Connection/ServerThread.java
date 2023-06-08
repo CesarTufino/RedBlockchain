65f5df81a59ee3069c8aa0bc4661e68f704007c1
JorgeCordero666 <jorge666.cordero@gmail.com>
2023-06-07 21:08:17 -0500

    Archivos txt

--- src/Connection/ServerThread.java
+++ src/Connection/ServerThread.java
@@ -13,13 +13,19 @@ public class ServerThread extends Thread {
     public void run() {
         while (true) {
             try {
+
                 Socket socket = serverSocket.accept();
                 System.out.println("Conexión aceptada");
+                BufferedWriter archivo = new BufferedWriter(new FileWriter("mensajes.txt", true));
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String mensaje;
                 while ((mensaje = in.readLine()) != null) {
                     System.out.println("Mensaje: " + mensaje);
+                    archivo.write(mensaje);
+                    archivo.newLine();
                 }
+                archivo.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
