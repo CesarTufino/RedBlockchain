43968b4edc3c213c9c07a3e0d37ce0b5c5083b2c
CesarTufino <cesar.tufino@epn.edu.ec>
2023-08-24 21:17:54 -0500

    Reduccion a dos nodos

--- src/main/java/blockchainTradicional/nodo/Nodo.java
+++ src/main/java/blockchainTradicional/nodo/Nodo.java
@@ -252,7 +252,7 @@ public class Nodo {
 
     public boolean comprobarCantidadMinimaDeNodos() {
         // System.out.println(infoRed.comprobarCantidadDeNodos());
-        if (red.obtenerCantidadDeNodos() >= 3) {
+        if (red.obtenerCantidadDeNodos() >= 2) {
             return true;
         }
         return false;
