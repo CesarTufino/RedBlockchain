28131f22496eb406423f53eed18a38a476242303
CesarTufino <cesar.tufino@epn.edu.ec>
2023-08-24 21:46:08 -0500

    Actualización validadores

--- src/main/java/blockchainTradicional/test/TestRed1Nodo3.java
+++ src/main/java/blockchainTradicional/test/TestRed1Nodo3.java
@@ -23,7 +23,7 @@ public class TestRed1Nodo3 {
             System.out.print("");
         }
         // Hilo para validación PoS
-        Validador hiloValidador = new Validador(nodo.getRed(), nodo);
+        Validador2 hiloValidador = new Validador2(nodo.getRed(), nodo);
         hiloValidador.start();
         // Generación de transacciones
         for (int i = 0; i < 700; i++) {
